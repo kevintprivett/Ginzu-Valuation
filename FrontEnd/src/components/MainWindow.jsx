76bb28f1d9f19c391118058af0eeb8f588bfa508
kevintprivett <kevintprivett@gmail.com>
2026-01-09 19:36:46 +0000

    Change from BrowserRouter to HashRouter to work with github pages

--- FrontEnd/src/components/MainWindow.jsx
+++ FrontEnd/src/components/MainWindow.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router,
+import { HashRouter as Router,
   Routes, Route, Link, Navigate, useLocation
 } from 'react-router-dom'
 import {
