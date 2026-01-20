a67bb8c5abde652bb6e287eadc155761f9c9e464
kevintprivett <kevintprivett@gmail.com>
2026-01-20 20:33:21 +0000

    test PR with lint and unit test failures.

--- FrontEnd/src/components/Footer.test.jsx
+++ FrontEnd/src/components/Footer.test.jsx
@@ -1,13 +1,15 @@
 import { screen } from '@testing-library/react'
 
 import { renderWithProvider } from '../utils/testUtils.jsx'
-import Footer from './Footer'
+// import Footer from './Footer'
 
 
 describe('Footer', async () => {
   beforeEach(() => {
     renderWithProvider(<Footer />)
   })
+
+  const unusedVar = 'test'
 
   test('component renders', () => {
     const element = screen.getByText(
