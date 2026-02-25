95f9644c5f8140ea9c577ef6e425b0b7561c6193
kevintprivett <kevintprivett@gmail.com>
2026-02-25 16:53:09 +0000

    linting: Removed TODO

--- FrontEnd/eslint.config.js
+++ FrontEnd/eslint.config.js
@@ -1,6 +1,5 @@
 import js from '@eslint/js'
 import globals from 'globals'
-// TODO: add back when fixed (version 7.2.X)
 // import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
