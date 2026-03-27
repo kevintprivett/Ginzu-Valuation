92f7aed4294dfe47fa3be948050fda8b263773a2
kevintprivett <kevintprivett@gmail.com>
2026-03-27 19:49:53 +0000

    Add in memory caching for rfr

--- BackEnd/services/DatabaseService.js
+++ BackEnd/services/DatabaseService.js
@@ -1,8 +1,30 @@
 import Database from 'better-sqlite3'
+import { NodeCache } from '@cacheable/node-cache'
 
 const db = new Database(process.env.SQLITE_DB, { verbose: console.log })
-
 db.pragma('journal_mode = WAL')
+
+const cache = new NodeCache({
+  stdTTL: 60*60*1000, // 1 hour
+});
+
+// TODO: move to a util class?
+const cacheCall = (key, call) => {
+  const cached = cache.get(key)
+
+  if (cached) {
+    console.log(`cache hit: ${key}`)
+    return cached
+  }
+
+  console.log(`cache miss: ${key}`)
+
+  const val = call()
+
+  cache.set('getRfr', val)
+
+  return val
+}
 
 const getRfrStmt = db.prepare(`
   SELECT *
@@ -12,5 +34,7 @@ const getRfrStmt = db.prepare(`
 `)
 
 export const getRfr = () => {
-  return getRfrStmt.get().rate
+  return cacheCall('getRfr', () => {
+    return getRfrStmt.get().rate
+  })
 }
